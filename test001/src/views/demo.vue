96a165ffc0a53a4ef1c5cb9f48f2545899bed82f
yeqin <1099239460@qq.com>
2021-08-01 20:30:19 +0800

    添加111

--- test001/src/views/demo.vue
+++ test001/src/views/demo.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+  <div>
     <!-- <el-select class="width100" v-model="form.nowname" clearable   
               filterable 
               allow-create 
@@ -10,40 +10,37 @@
   <el-option v-for="item in options" :key="item.key" :label="item.labe l" :value="item.label"></el-option>
 </el-select>
 <el-button @click="dianji">ddd</el-button> -->
-<el-button @click="dianji">ddd</el-button>
-
-    </div>
+    <el-button @click="dianji">ddd</el-button>
+    1111
+  </div>
 </template>
 <script>
 export default {
-    name: "goTop",
-    data() {
-        return {
-            isa:'a231'
-        //      options: [{
-        //   value: '测试1',
-        //   label: '测试'
-        // }],
-        // form:{
-        //     nowname:''
-        // },
-        // value: []
-        
+  name: "goTop",
+  data() {
+    return {
+      isa: "a231",
+      //      options: [{
+      //   value: '测试1',
+      //   label: '测试'
+      // }],
+      // form:{
+      //     nowname:''
+      // },
+      // value: []
+    };
+  },
 
-        };
-    },
- 
-    methods: {
-      dianji(){
+  methods: {
+    dianji() {
       var sSrc = "a33333";
-if(typeof sSrc==='string'){
-    var sASC = sSrc.charCodeAt(0);
-if((sASC >= 65 && sASC <= 90) || (sASC >= 97 && sASC <= 122)){
-alert('我首位是子母')
-}
-}
-
+      if (typeof sSrc === "string") {
+        var sASC = sSrc.charCodeAt(0);
+        if ((sASC >= 65 && sASC <= 90) || (sASC >= 97 && sASC <= 122)) {
+          alert("我首位是子母");
+        }
       }
     },
-}
+  },
+};
 </script>
